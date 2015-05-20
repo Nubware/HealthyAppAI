2dc13a74f46c8ffd43eeff3cd077179779a3d148
unknown-user <unknown-user@fe80::70a1:45b1:f701:211e%34>
2015-05-20 12:34:29 -0500

    a

--- HealthyAppAI/MainActivity.cs
+++ HealthyAppAI/MainActivity.cs
@@ -17,6 +17,7 @@ namespace HealthyAppAI
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate(bundle);
+			string str; 
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
